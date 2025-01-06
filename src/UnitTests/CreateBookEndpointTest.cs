ac7be9effe02da349ba5539150c429f0cf6740cd
Marvin Thomaz do Nascimento <marvin.nascimento@invillia.com>
2025-01-06 09:04:01 -0300

    Separa os testes para que cada teste tenha apenas um assert

--- src/UnitTests/CreateBookEndpointTest.cs
+++ src/UnitTests/CreateBookEndpointTest.cs
@@ -9,12 +9,11 @@ namespace UnitTests;
 public class CreateBookEndpointTest
 {
     [Fact]
-    public async Task GivenAValidBook_WhenCreating_ThenReturnsSuccess()
+    public async Task GivenAValidBook_WhenCreating_ThenInsertsBook()
     {
         //Arrange
         var request = new Api.Features.Book.Create.Request(
-            Title:
-            "The mockRepository.Verify(r => r.InsertAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()), Times.Once);Galactic Codex of Time",
+            Title: "Galactic Codex of Time",
             Author: "Chrono Voyager",
             ISBN: "987-65-43210-XYZ",
             Publisher: "Transdimensional Press",
@@ -33,8 +32,40 @@ public class CreateBookEndpointTest
             .Callback<Book, CancellationToken>((book, token) => capturedBook = book)
             .Returns(Task.CompletedTask);
 
-        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object,
-            mockLogger.Object);
+        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object, mockLogger.Object);
+
+        //Act
+        await endpoint.HandleAsync(request, CancellationToken.None);
+
+        //Assert
+        mockRepository.Verify(r => r.InsertAsync(capturedBook, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task GivenAValidBook_WhenCreating_ThenResponseIsNotNull()
+    {
+        //Arrange
+        var request = new Api.Features.Book.Create.Request(
+            Title: "Galactic Codex of Time",
+            Author: "Chrono Voyager",
+            ISBN: "987-65-43210-XYZ",
+            Publisher: "Transdimensional Press",
+            PublishedDate: new DateTime(2088, 7, 14),
+            Pages: 999,
+            AvailableCopies: 13,
+            TotalCopies: 42,
+            Genre: "Cosmic Sci-Fi"
+        );
+
+        var mockRepository = new Mock<IBookRepository>();
+        var mockLogger = new Mock<ILogger<Api.Features.Book.Create.Endpoint>>();
+        var capturedBook = new Book();
+
+        mockRepository.Setup(r => r.InsertAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+            .Callback<Book, CancellationToken>((book, token) => capturedBook = book)
+            .Returns(Task.CompletedTask);
+
+        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object, mockLogger.Object);
 
         //Act
         await endpoint.HandleAsync(request, CancellationToken.None);
@@ -42,20 +73,338 @@ public class CreateBookEndpointTest
         //Assert
         var response = endpoint.Response;
 
-        mockRepository.Verify(r => r.InsertAsync(capturedBook, It.IsAny<CancellationToken>()), Times.Once);
-
         Assert.NotNull(response);
+    }
+
+    [Fact]
+    public async Task GivenAValidBook_WhenCreating_ThenResponseIdMatchesBookId()
+    {
+        //Arrange
+        var request = new Api.Features.Book.Create.Request(
+            Title: "Galactic Codex of Time",
+            Author: "Chrono Voyager",
+            ISBN: "987-65-43210-XYZ",
+            Publisher: "Transdimensional Press",
+            PublishedDate: new DateTime(2088, 7, 14),
+            Pages: 999,
+            AvailableCopies: 13,
+            TotalCopies: 42,
+            Genre: "Cosmic Sci-Fi"
+        );
+
+        var mockRepository = new Mock<IBookRepository>();
+        var mockLogger = new Mock<ILogger<Api.Features.Book.Create.Endpoint>>();
+        var capturedBook = new Book();
+
+        mockRepository.Setup(r => r.InsertAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+            .Callback<Book, CancellationToken>((book, token) => capturedBook = book)
+            .Returns(Task.CompletedTask);
+
+        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object, mockLogger.Object);
+
+        //Act
+        await endpoint.HandleAsync(request, CancellationToken.None);
+
+        //Assert
+        var response = endpoint.Response;
 
         Assert.Equal(response.Id, capturedBook.Id);
+    }
 
+    [Fact]
+    public async Task GivenAValidBook_WhenCreating_ThenBookTitleMatchesRequest()
+    {
+        //Arrange
+        var request = new Api.Features.Book.Create.Request(
+            Title: "Galactic Codex of Time",
+            Author: "Chrono Voyager",
+            ISBN: "987-65-43210-XYZ",
+            Publisher: "Transdimensional Press",
+            PublishedDate: new DateTime(2088, 7, 14),
+            Pages: 999,
+            AvailableCopies: 13,
+            TotalCopies: 42,
+            Genre: "Cosmic Sci-Fi"
+        );
+
+        var mockRepository = new Mock<IBookRepository>();
+        var mockLogger = new Mock<ILogger<Api.Features.Book.Create.Endpoint>>();
+        var capturedBook = new Book();
+
+        mockRepository.Setup(r => r.InsertAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+            .Callback<Book, CancellationToken>((book, token) => capturedBook = book)
+            .Returns(Task.CompletedTask);
+
+        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object, mockLogger.Object);
+
+        //Act
+        await endpoint.HandleAsync(request, CancellationToken.None);
+
+        //Assert
         Assert.Equal(request.Title, capturedBook.Title);
+    }
+
+    [Fact]
+    public async Task GivenAValidBook_WhenCreating_ThenBookAuthorMatchesRequest()
+    {
+        //Arrange
+        var request = new Api.Features.Book.Create.Request(
+            Title: "Galactic Codex of Time",
+            Author: "Chrono Voyager",
+            ISBN: "987-65-43210-XYZ",
+            Publisher: "Transdimensional Press",
+            PublishedDate: new DateTime(2088, 7, 14),
+            Pages: 999,
+            AvailableCopies: 13,
+            TotalCopies: 42,
+            Genre: "Cosmic Sci-Fi"
+        );
+
+        var mockRepository = new Mock<IBookRepository>();
+        var mockLogger = new Mock<ILogger<Api.Features.Book.Create.Endpoint>>();
+        var capturedBook = new Book();
+
+        mockRepository.Setup(r => r.InsertAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+            .Callback<Book, CancellationToken>((book, token) => capturedBook = book)
+            .Returns(Task.CompletedTask);
+
+        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object, mockLogger.Object);
+
+        //Act
+        await endpoint.HandleAsync(request, CancellationToken.None);
+
+        //Assert
         Assert.Equal(request.Author, capturedBook.Author);
+    }
+
+    [Fact]
+    public async Task GivenAValidBook_WhenCreating_ThenBookISBNMatchesRequest()
+    {
+        //Arrange
+        var request = new Api.Features.Book.Create.Request(
+            Title: "Galactic Codex of Time",
+            Author: "Chrono Voyager",
+            ISBN: "987-65-43210-XYZ",
+            Publisher: "Transdimensional Press",
+            PublishedDate: new DateTime(2088, 7, 14),
+            Pages: 999,
+            AvailableCopies: 13,
+            TotalCopies: 42,
+            Genre: "Cosmic Sci-Fi"
+        );
+
+        var mockRepository = new Mock<IBookRepository>();
+        var mockLogger = new Mock<ILogger<Api.Features.Book.Create.Endpoint>>();
+        var capturedBook = new Book();
+
+        mockRepository.Setup(r => r.InsertAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+            .Callback<Book, CancellationToken>((book, token) => capturedBook = book)
+            .Returns(Task.CompletedTask);
+
+        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object, mockLogger.Object);
+
+        //Act
+        await endpoint.HandleAsync(request, CancellationToken.None);
+
+        //Assert
         Assert.Equal(request.ISBN, capturedBook.ISBN);
+    }
+
+    [Fact]
+    public async Task GivenAValidBook_WhenCreating_ThenBookPublisherMatchesRequest()
+    {
+        //Arrange
+        var request = new Api.Features.Book.Create.Request(
+            Title: "Galactic Codex of Time",
+            Author: "Chrono Voyager",
+            ISBN: "987-65-43210-XYZ",
+            Publisher: "Transdimensional Press",
+            PublishedDate: new DateTime(2088, 7, 14),
+            Pages: 999,
+            AvailableCopies: 13,
+            TotalCopies: 42,
+            Genre: "Cosmic Sci-Fi"
+        );
+
+        var mockRepository = new Mock<IBookRepository>();
+        var mockLogger = new Mock<ILogger<Api.Features.Book.Create.Endpoint>>();
+        var capturedBook = new Book();
+
+        mockRepository.Setup(r => r.InsertAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+            .Callback<Book, CancellationToken>((book, token) => capturedBook = book)
+            .Returns(Task.CompletedTask);
+
+        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object, mockLogger.Object);
+
+        //Act
+        await endpoint.HandleAsync(request, CancellationToken.None);
+
+        //Assert
         Assert.Equal(request.Publisher, capturedBook.Publisher);
+    }
+
+    [Fact]
+    public async Task GivenAValidBook_WhenCreating_ThenBookPublishedDateMatchesRequest()
+    {
+        //Arrange
+        var request = new Api.Features.Book.Create.Request(
+            Title: "Galactic Codex of Time",
+            Author: "Chrono Voyager",
+            ISBN: "987-65-43210-XYZ",
+            Publisher: "Transdimensional Press",
+            PublishedDate: new DateTime(2088, 7, 14),
+            Pages: 999,
+            AvailableCopies: 13,
+            TotalCopies: 42,
+            Genre: "Cosmic Sci-Fi"
+        );
+
+        var mockRepository = new Mock<IBookRepository>();
+        var mockLogger = new Mock<ILogger<Api.Features.Book.Create.Endpoint>>();
+        var capturedBook = new Book();
+
+        mockRepository.Setup(r => r.InsertAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+            .Callback<Book, CancellationToken>((book, token) => capturedBook = book)
+            .Returns(Task.CompletedTask);
+
+        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object, mockLogger.Object);
+
+        //Act
+        await endpoint.HandleAsync(request, CancellationToken.None);
+
+        //Assert
         Assert.Equal(request.PublishedDate, capturedBook.PublishedDate);
+    }
+
+    [Fact]
+    public async Task GivenAValidBook_WhenCreating_ThenBookPagesMatchRequest()
+    {
+        //Arrange
+        var request = new Api.Features.Book.Create.Request(
+            Title: "Galactic Codex of Time",
+            Author: "Chrono Voyager",
+            ISBN: "987-65-43210-XYZ",
+            Publisher: "Transdimensional Press",
+            PublishedDate: new DateTime(2088, 7, 14),
+            Pages: 999,
+            AvailableCopies: 13,
+            TotalCopies: 42,
+            Genre: "Cosmic Sci-Fi"
+        );
+
+        var mockRepository = new Mock<IBookRepository>();
+        var mockLogger = new Mock<ILogger<Api.Features.Book.Create.Endpoint>>();
+        var capturedBook = new Book();
+
+        mockRepository.Setup(r => r.InsertAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+            .Callback<Book, CancellationToken>((book, token) => capturedBook = book)
+            .Returns(Task.CompletedTask);
+
+        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object, mockLogger.Object);
+
+        //Act
+        await endpoint.HandleAsync(request, CancellationToken.None);
+
+        //Assert
         Assert.Equal(request.Pages, capturedBook.Pages);
+    }
+
+    [Fact]
+    public async Task GivenAValidBook_WhenCreating_ThenBookAvailableCopiesMatchRequest()
+    {
+        //Arrange
+        var request = new Api.Features.Book.Create.Request(
+            Title: "Galactic Codex of Time",
+            Author: "Chrono Voyager",
+            ISBN: "987-65-43210-XYZ",
+            Publisher: "Transdimensional Press",
+            PublishedDate: new DateTime(2088, 7, 14),
+            Pages: 999,
+            AvailableCopies: 13,
+            TotalCopies: 42,
+            Genre: "Cosmic Sci-Fi"
+        );
+
+        var mockRepository = new Mock<IBookRepository>();
+        var mockLogger = new Mock<ILogger<Api.Features.Book.Create.Endpoint>>();
+        var capturedBook = new Book();
+
+        mockRepository.Setup(r => r.InsertAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+            .Callback<Book, CancellationToken>((book, token) => capturedBook = book)
+            .Returns(Task.CompletedTask);
+
+        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object, mockLogger.Object);
+
+        //Act
+        await endpoint.HandleAsync(request, CancellationToken.None);
+
+        //Assert
         Assert.Equal(request.AvailableCopies, capturedBook.AvailableCopies);
+    }
+
+    [Fact]
+    public async Task GivenAValidBook_WhenCreating_ThenBookTotalCopiesMatchRequest()
+    {
+        //Arrange
+        var request = new Api.Features.Book.Create.Request(
+            Title: "Galactic Codex of Time",
+            Author: "Chrono Voyager",
+            ISBN: "987-65-43210-XYZ",
+            Publisher: "Transdimensional Press",
+            PublishedDate: new DateTime(2088, 7, 14),
+            Pages: 999,
+            AvailableCopies: 13,
+            TotalCopies: 42,
+            Genre: "Cosmic Sci-Fi"
+        );
+
+        var mockRepository = new Mock<IBookRepository>();
+        var mockLogger = new Mock<ILogger<Api.Features.Book.Create.Endpoint>>();
+        var capturedBook = new Book();
+
+        mockRepository.Setup(r => r.InsertAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+            .Callback<Book, CancellationToken>((book, token) => capturedBook = book)
+            .Returns(Task.CompletedTask);
+
+        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object, mockLogger.Object);
+
+        //Act
+        await endpoint.HandleAsync(request, CancellationToken.None);
+
+        //Assert
         Assert.Equal(request.TotalCopies, capturedBook.TotalCopies);
+    }
+
+    [Fact]
+    public async Task GivenAValidBook_WhenCreating_ThenBookGenreMatchesRequest()
+    {
+        //Arrange
+        var request = new Api.Features.Book.Create.Request(
+            Title: "Galactic Codex of Time",
+            Author: "Chrono Voyager",
+            ISBN: "987-65-43210-XYZ",
+            Publisher: "Transdimensional Press",
+            PublishedDate: new DateTime(2088, 7, 14),
+            Pages: 999,
+            AvailableCopies: 13,
+            TotalCopies: 42,
+            Genre: "Cosmic Sci-Fi"
+        );
+
+        var mockRepository = new Mock<IBookRepository>();
+        var mockLogger = new Mock<ILogger<Api.Features.Book.Create.Endpoint>>();
+        var capturedBook = new Book();
+
+        mockRepository.Setup(r => r.InsertAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+            .Callback<Book, CancellationToken>((book, token) => capturedBook = book)
+            .Returns(Task.CompletedTask);
+
+        var endpoint = Factory.Create<Api.Features.Book.Create.Endpoint>(mockRepository.Object, mockLogger.Object);
+
+        //Act
+        await endpoint.HandleAsync(request, CancellationToken.None);
+
+        //Assert
         Assert.Equal(request.Genre, capturedBook.Genre);
     }
 
@@ -83,6 +432,7 @@ public class CreateBookEndpointTest
     [Fact]
     public async Task GivenABookWithNegativePages_WhenCreating_ThenReturnsError()
     {
+        //Arrange
         var request = new Api.Features.Book.Create.Request(
             Title: "Negative Pages Book",
             Author: "Author",
@@ -96,8 +446,11 @@ public class CreateBookEndpointTest
         );
 
         var validator = new Api.Features.Book.Create.Validator();
+        
+        //Act
         var validationResult = await validator.ValidateAsync(request, CancellationToken.None);
 
+        //Assert
         Assert.False(validationResult.IsValid);
     }
 }
